7e77210743796c42b02b0beec8ee120f4dac1908
Nils Langner <contact@phmlabs.com>
2016-01-16 11:44:38 +0100

    switchedvto new plugin interface

--- src/EventListener/PluginListener.php
+++ src/EventListener/PluginListener.php
@@ -2,26 +2,23 @@
 
 namespace Koalamon\NotificationBundle\EventListener;
 
-use Bauer\IncidentDashboard\CoreBundle\Entity\Event;
+use Bauer\IncidentDashboard\CoreBundle\Entity\Project;
+use Koalamon\DefaultBundle\EventListener\AdminMenuEvent;
 use Koalamon\DefaultBundle\Menu\Element;
-use Koalamon\DefaultBundle\Menu\Menu;
-use Koalamon\NotificationBundle\Sender\SenderFactory;
+use Koalamon\IntegrationBundle\EventListener\PluginInitEvent;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class AdminMenuListener
+class PluginListener
 {
-    private $router;
-
     public function __construct(ContainerInterface $container)
     {
         $this->router = $container->get('Router');
     }
 
-    public function onAdminMenu(\Symfony\Component\EventDispatcher\Event $event)
+    public function onKoalamonPluginAdminMenuInit(AdminMenuEvent $event)
     {
         $menu = $event->getMenu();
         $project = $event->getProject();
-        /** @var Menu $menu */
 
         $menu->addElement(new Element($this->router->generate('koalamon_notification_home', ['project' => $project->getIdentifier()], true),
             'Notification Channels', 'menu_admin_notification_channels'));
